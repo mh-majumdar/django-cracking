948cfea10be04d8b156b38109649d5df5cff99e1
Mehedi Hasan <66936454+mh-majumdar@users.noreply.github.com>
2024-01-21 12:11:25 +0600

    firstproject

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'firstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templetes'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
